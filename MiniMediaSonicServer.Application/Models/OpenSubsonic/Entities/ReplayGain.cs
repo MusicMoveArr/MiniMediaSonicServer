bf6d7990e1b80586b35f64233470065a444e405f
MusicMoveArr <jys78dfg@bottlepost.me>
2026-01-29 17:19:49 +0100

    ReplayGain Xml serialization fix

--- MiniMediaSonicServer.Application/Models/OpenSubsonic/Entities/ReplayGain.cs
+++ MiniMediaSonicServer.Application/Models/OpenSubsonic/Entities/ReplayGain.cs
@@ -5,27 +5,27 @@ namespace MiniMediaSonicServer.Application.Models.OpenSubsonic.Entities;
 
 public class ReplayGain
 {
-    [XmlAttribute("trackGain")]
+    [XmlElement("trackGain")]
     [JsonPropertyName("trackGain")]
     public float? TrackGain { get; set; }
     
-    [XmlAttribute("albumGain")]
+    [XmlElement("albumGain")]
     [JsonPropertyName("albumGain")]
     public float? AlbumGain { get; set; }
     
-    [XmlAttribute("trackPeak")]
+    [XmlElement("trackPeak")]
     [JsonPropertyName("trackPeak")]
     public float? TrackPeak { get; set; }
     
-    [XmlAttribute("albumPeak")]
+    [XmlElement("albumPeak")]
     [JsonPropertyName("albumPeak")]
     public float? AlbumPeak { get; set; }
     
-    [XmlAttribute("baseGain")]
+    [XmlElement("baseGain")]
     [JsonPropertyName("baseGain")]
     public float? BaseGain { get; set; }
     
-    [XmlAttribute("fallbackGain")]
+    [XmlElement("fallbackGain")]
     [JsonPropertyName("fallbackGain")]
     public float? FallbackGain { get; set; }
 }
