427efa046d0da100f98a29ab25920a4f5e82a9ac
MusicMoveArr <jys78dfg@bottlepost.me>
2026-03-28 10:11:32 +0100

    Updated GetArtist Query to add Duration, LastPlayDate, AlbumPlaycount

--- MiniMediaSonicServer.Application/Repositories/ArtistRepository.cs
+++ MiniMediaSonicServer.Application/Repositories/ArtistRepository.cs
@@ -35,20 +35,40 @@ public class ArtistRepository
 						 	al.Title as Name,
 						 	'' as version,
 						 	a.Name as Artist,
-						 	NULLIF(m.tag_year, 0) as year,
 						 	'album_' || al.AlbumId as CoverArt,
  							a.artistid AS ArtistId,
-							m.file_creationtime as Created,
- 							songCount.songs as songCount
+							al_sum.file_creationtime as Created,
+							al_sum.Duration,
+							al_sum.SongCount,
+							al_sum.Year,
+							al_sum.LastPlayDate as Played,
+							al_sum.AlbumPlaycount as PlayCount
 						 FROM artists a
 						 JOIN albums al ON al.artistid = a.artistid
  						 left join sonicserver_artist_rated artist_rated on artist_rated.ArtistId = a.ArtistId and artist_rated.UserId = @userId
-						 JOIN lateral (select * from metadata m where m.albumid = al.albumid order by m.tag_year desc limit 1) as m on true
 						 JOIN lateral (select count(ab.albumid) as albums from albums ab where ab.artistid = a.artistid limit 1) as album_count on true
-						 LEFT JOIN lateral (
-							select count(*) songs 
-							from metadata m 
-							where m.albumid = al.albumid) as songCount on true
+						 JOIN lateral (
+						     select 
+								    min(m.file_creationtime) as file_creationtime,
+								    nullif(max(m.tag_year), 0) as Year, 
+								    sum(EXTRACT(EPOCH FROM
+									    (CASE WHEN length(m.Tag_Length) = 5 THEN '00:' || m.Tag_Length 
+									    	ELSE m.Tag_Length END)::interval))::int as Duration,
+						     		max(hist.UpdatedAt) as LastPlayDate,
+									sum(hist.PlayCount) as AlbumPlaycount,
+								    count(distinct(m.MetadataId)) as SongCount
+							 from metadata m 
+						     left join (
+						         select TrackId, max(UpdatedAt) as UpdatedAt, count(*) as PlayCount
+						         from sonicserver_user_playhistory
+						         where UserId = @userId
+						         group by TrackId
+						     ) hist on hist.TrackId = m.MetadataId
+						     where m.albumid = al.albumid
+						     ) as al_sum on true
+						                
+						                
+						                
 						 where a.ArtistId = @artistId";
 
         await using var conn = new NpgsqlConnection(_databaseConfiguration.ConnectionString);
