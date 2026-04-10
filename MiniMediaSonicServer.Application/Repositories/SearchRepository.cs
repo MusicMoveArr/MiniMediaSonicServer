7b901469ecce979f32f0d06b4a75acfdfe43794e
MusicMoveArr <jys78dfg@bottlepost.me>
2026-04-10 17:53:26 +0200

    Improved Search queries

--- MiniMediaSonicServer.Application/Repositories/SearchRepository.cs
+++ MiniMediaSonicServer.Application/Repositories/SearchRepository.cs
@@ -20,25 +20,32 @@ public class SearchRepository
     public async Task<List<ArtistID3>> SearchArtistsAsync(string searchquery, int count, int offset, Guid userId)
     {
 	    string query = @"SET LOCAL pg_trgm.similarity_threshold = 0.5;
-						 SELECT distinct on (a.record_id)
-    						a.ArtistId as Id,
-    						a.Name as Name,
-    						'artist_' || a.ArtistId as CoverArt,
-    						'' as ArtistImageUrl,
-    						album_count.albums as AlbumCount,
- 							artist_rated.Rating as UserRating,
- 							(case when artist_rated.Starred = true 
- 							    then artist_rated.StarredAt 
- 							    else null 
- 							 end) as Starred
-						 FROM artists a
-						 left join sonicserver_indexed_search search on search.Id = a.ArtistId
- 						 left join sonicserver_artist_rated artist_rated on artist_rated.ArtistId = a.ArtistId and artist_rated.UserId = @userId
-						 JOIN albums al ON al.artistid = a.artistid
-						 JOIN lateral (select * from metadata m where m.albumid = al.albumid order by m.tag_year desc limit 1) as m on true
-						 JOIN lateral (select count(ab.albumid) as albums from albums ab where ab.artistid = a.artistid limit 1) as album_count on true
+						 SELECT
+							 a.ArtistId as Id,
+							 a.Name as Name,
+							 'artist_' || a.ArtistId as CoverArt,
+							 '' as ArtistImageUrl,
+							 album_count.albums as AlbumCount,
+							 artist_rated.Rating as UserRating,
+							 (case when artist_rated.Starred = true 
+								 then artist_rated.StarredAt 
+								 else null 
+							 end) as Starred,
+							 a.record_id
+						 FROM sonicserver_indexed_search search 
+						 join artists a on a.ArtistId = search.Id
+						 left join sonicserver_artist_rated artist_rated on 
+							 artist_rated.ArtistId = a.ArtistId 
+							 and artist_rated.UserId = @userId
+						 JOIN lateral (
+							 select count(ab.albumid) as albums 
+							 from albums ab 
+							 where ab.artistid = a.artistid
+						 ) as album_count on true
+ 
 						 where search.SearchTerm % lower(@searchquery)
-					     offset @offset
+						 order by similarity(search.SearchTerm, lower(@searchquery)) desc
+						 offset @offset
 						 limit @count";
 
         await using var conn = new NpgsqlConnection(_databaseConfiguration.ConnectionString);
@@ -73,39 +80,40 @@ public class SearchRepository
     public async Task<List<AlbumID3>> SearchAlbumsAsync(string searchquery, int count, int offset, Guid userId)
     {
 	    string query = @"SET LOCAL pg_trgm.similarity_threshold = 0.5;
-						 SELECT distinct on (al.AlbumId)
-						 	al.AlbumId as Id,
-						 	al.Title as Name,
-						 	'' as version,
-						 	a.Name as Artist,
-						 	NULLIF(m.tag_year, 0) as year,
-						 	'album_' || al.AlbumId as CoverArt,
- 							a.artistid AS ArtistId,
-							m.file_creationtime as Created,
- 							songCount.songs as songCount,
- 							album_rated.Rating as UserRating,
- 							(case when album_rated.Starred = true 
- 							    then album_rated.StarredAt 
- 							    else null 
- 							 end) as Starred,
+						 SELECT
+ 							al.AlbumId as Id,
+ 							al.Title as Name,
+ 							'' as version,
+ 							a.Name as Artist,
+ 							al_sum.Year as year,
+ 							'album_' || al.AlbumId as CoverArt,
+							a.artistid AS ArtistId,
+							al_sum.file_creationtime_min as Created,
+							al_sum.SongCount as songCount,
+							album_rated.Rating as UserRating,
+							(case when album_rated.Starred = true 
+							    then album_rated.StarredAt 
+							    else null 
+							 end) as Starred,
 							search.SearchTerm
-						 FROM albums al
-						 join sonicserver_indexed_search search on search.Id = al.AlbumId
+						 FROM sonicserver_indexed_search search
+						 join albums al on al.AlbumId = search.Id
 						 JOIN artists a on a.ArtistId = al.ArtistId
- 						 left join sonicserver_album_rated album_rated on album_rated.AlbumId = al.AlbumId and album_rated.UserId = @userId
-						 LEFT JOIN lateral (select * from metadata m where m.albumid = al.albumid order by m.tag_year desc limit 1) as m on true
+						 left join sonicserver_album_rated album_rated on 
+ 							album_rated.AlbumId = al.AlbumId 
+ 							and album_rated.UserId = @userId
+ 							
 						 LEFT JOIN lateral (
-							select count(*) songs 
-							from metadata m 
-							where m.albumid = al.albumid) as songCount on true
-
-						 LEFT JOIN lateral (
-						     select m.file_creationtime as file_creationtime 
-						     from metadata m 
-						     where m.albumid = al.albumid 
-						     order by m.file_creationtime desc
-						     limit 1) as recent_m on true
+							 select
+	 							nullif(max(m.tag_year), 0) as Year, 
+	 							min(m.file_creationtime) as file_creationtime_min,
+	 							count(distinct(m.MetadataId)) as SongCount
+							 from metadata m 
+							 where m.albumid = al.albumid
+							 ) as al_sum on true
+						     
 						 where search.SearchTerm % lower(@searchquery)
+						 order by similarity(search.SearchTerm, lower(@searchquery)) desc
 						 offset @offset
 						 limit @count";
 
@@ -142,94 +150,90 @@ public class SearchRepository
     {
 	    string query = @$"SET LOCAL pg_trgm.similarity_threshold = 0.{accuracy};
 						 SELECT 
- 							m.MetadataId as TrackId,
- 							al.AlbumId as Parent,
- 							al.AlbumId as AlbumId,
- 							a.artistid AS ArtistId,
-						 	'album_' || m.MetadataId as CoverArt,
- 							m.Title as Title,
- 							al.Title as Album,
- 							a.Name as Artist,
- 							m.Tag_Track as TrackNumber,
- 							NULLIF(m.tag_year, 0) as Year,
- 							m.Computed_Genre as Genre,
- 							0 as Size,
- 							case 
- 								when m.Path ilike '%.mp3' then 'audio/mpeg'
- 								when m.Path ilike '%.m4a' then 'audio/mp4'
- 								when m.Path ilike '%.mp4' then 'audio/mp4'
- 								when m.Path ilike '%.flac' then 'audio/flac'
- 								when m.Path ilike '%.ogg' then 'audio/ogg'
- 								when m.Path ilike '%.opus' then 'audio/ogg'
- 								when m.Path ilike '%.wav' then 'audio/wav'
- 								else 'application/octet-stream'
- 							end as ContentType,
- 							regexp_substr(m.Path, '([a-zA-Z0-9]{{2,5}})$') as Suffix,
-							m.tag_isrc as Isrc_Single,
-							m.Path as Path,
-							'music' AS Type,
-							'song' AS MediaType,
- 							playhistory.LastPlayDate as Played,
- 							    
- 							EXTRACT(EPOCH FROM
-							    (CASE WHEN length(m.Tag_Length) = 5 THEN '00:' || m.Tag_Length 
-							    ELSE m.Tag_Length END)::interval) AS Duration,
- 							    
-							m.file_creationtime as Created,
-							t.tags->>'bitrate' AS BitRate,
-							16 as BitDepth,
-							44100 as SamplingRate,
-							2 as ChannelCount,
+							 m.MetadataId as TrackId,
+							 al.AlbumId as Parent,
+							 al.AlbumId as AlbumId,
+							 a.artistid AS ArtistId,
+							 'album_' || m.MetadataId as CoverArt,
+							 m.Title as Title,
+							 al.Title as Album,
+							 a.Name as Artist,
+							 m.Tag_Track as TrackNumber,
+							 NULLIF(m.tag_year, 0) as Year,
+							 m.Computed_Genre as Genre,
+							 0 as Size,
+							 case when m.Path ilike '%.mp3' then 'audio/mpeg'
+								  when m.Path ilike '%.m4a' then 'audio/mp4'
+								  when m.Path ilike '%.mp4' then 'audio/mp4'
+								  when m.Path ilike '%.flac' then 'audio/flac'
+								  when m.Path ilike '%.ogg' then 'audio/ogg'
+								  when m.Path ilike '%.opus' then 'audio/ogg'
+								  when m.Path ilike '%.wav' then 'audio/wav'
+								  else 'application/octet-stream'
+							 end as ContentType,
+							 regexp_substr(m.Path, '([a-zA-Z0-9]{{2,5}})$') as Suffix,
+							 m.tag_isrc as Isrc_Single,
+							 m.Path as Path,
+							 'music' AS Type,
+							 'song' AS MediaType,
+							 playhistory.LastPlayDate as Played,
 
- 							track_rated.Rating as UserRating,
- 							(case when track_rated.Starred = true 
- 							    then track_rated.StarredAt 
- 							    else null 
- 							 end) as Starred,
+							 EXTRACT(EPOCH FROM
+							 (CASE WHEN length(m.Tag_Length) = 5 THEN '00:' || m.Tag_Length 
+								ELSE m.Tag_Length END)::interval) AS Duration,
 
-							regexp_substr(t.tags->>'bpm', '[0-9]+([0-9]+)?') as BPM,
-							regexp_substr(t.tags->>'replaygain_track_gain', '-?[0-9]+(\.[0-9]+)?') as TrackGain,
-							regexp_substr(t.tags->>'replaygain_album_gain', '-?[0-9]+(\.[0-9]+)?') as AlbumGain,
-							regexp_substr(t.tags->>'replaygain_track_peak', '-?[0-9]+(\.[0-9]+)?') as TrackPeak,
-							regexp_substr(t.tags->>'replaygain_album_peak', '-?[0-9]+(\.[0-9]+)?') as AlbumPeak,
- 							    
-							joined_artist.ArtistId as Id,
-							joined_artist.Name
-						 FROM metadata m
+							 m.file_creationtime as Created,
+							 t.tags->>'bitrate' AS BitRate,
+							 16 as BitDepth,
+							 44100 as SamplingRate,
+							 2 as ChannelCount,
+
+							 track_rated.Rating as UserRating,
+							 (case when track_rated.Starred = true 
+								 then track_rated.StarredAt 
+								 else null 
+							 end) as Starred,
+
+							 regexp_substr(t.tags->>'bpm', '[0-9]+([0-9]+)?') as BPM,
+							 regexp_substr(t.tags->>'replaygain_track_gain', '-?[0-9]+(\.[0-9]+)?') as TrackGain,
+							 regexp_substr(t.tags->>'replaygain_album_gain', '-?[0-9]+(\.[0-9]+)?') as AlbumGain,
+							 regexp_substr(t.tags->>'replaygain_track_peak', '-?[0-9]+(\.[0-9]+)?') as TrackPeak,
+							 regexp_substr(t.tags->>'replaygain_album_peak', '-?[0-9]+(\.[0-9]+)?') as AlbumPeak,
+
+							 joined_artist.ArtistId as Id,
+							 joined_artist.Name
+						 FROM sonicserver_indexed_search search
+						 join metadata m on m.MetadataId = search.Id
 						 JOIN albums al ON al.AlbumId = m.AlbumId
 						 JOIN artists a on a.ArtistId = al.ArtistId
-						 left join sonicserver_indexed_search search on search.Id = m.MetadataId
-						 left join sonicserver_track_rated track_rated on track_rated.TrackId = m.MetadataId and track_rated.UserId = @userId
- 							    
- 						 left join lateral (
- 							select DISTINCT unnest(string_to_array(
-							        replace(replace(
-									            COALESCE(tag_alljsontags->>'Artists', tag_alljsontags->>'ARTISTS'), 
-									            '&', ';'), 
-									            '/', ';'),
-									        ';'
-									    )) AS artist
-						 ) all_artists ON true
- 						left join lateral (
- 							select artistid, name 
- 							from artists join_artist 
- 							where lower(join_artist.name) = lower(all_artists.artist)
- 							limit 1) joined_artist on true
- 							    
- 						 LEFT JOIN LATERAL (
-						    SELECT jsonb_object_agg(lower(key), value) AS tags
-						    FROM jsonb_each_text(m.tag_alljsontags)
-						  ) t ON TRUE
+						 left join sonicserver_track_rated track_rated on 
+						 track_rated.TrackId = m.MetadataId 
+						 and track_rated.UserId = @userId
 
- 						 left join lateral (
- 							select hist.UpdatedAt as LastPlayDate
- 							from sonicserver_user_playhistory hist
- 							where hist.UserId = @userId and hist.TrackId = m.MetadataId
- 							order by UpdatedAt desc
- 							limit 1) playhistory on true
+						 LEFT JOIN LATERAL (
+							 SELECT jsonb_object_agg(lower(key), value) AS tags
+							 FROM jsonb_each_text(m.tag_alljsontags)
+							 ) t ON true
+
+						 left join lateral (
+							select DISTINCT unnest(string_to_array(t.tags->>'artists', ';')) AS artist
+						 ) all_artists ON true
+
+						 left join lateral (
+							 select artistid, name 
+							 from artists join_artist 
+							 where lower(join_artist.name) = lower(all_artists.artist)
+							 limit 1) joined_artist on true
+
+						 left join lateral (
+							 select hist.UpdatedAt as LastPlayDate
+							 from sonicserver_user_playhistory hist
+							 where hist.UserId = @userId and hist.TrackId = m.MetadataId
+							 order by UpdatedAt desc
+							 limit 1) playhistory on true
 
 						 where search.SearchTerm % lower(@searchquery)
-					     order by similarity(search.SearchTerm, lower(@searchquery)) desc 
+						 order by similarity(search.SearchTerm, lower(@searchquery)) desc 
 						 offset @offset
 						 limit @count";
 
