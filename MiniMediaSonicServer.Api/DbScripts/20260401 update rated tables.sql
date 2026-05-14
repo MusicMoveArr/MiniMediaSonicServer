65b201109e4def8b2fe513771be991bb8cd7cf2b
MusicMoveArr <jys78dfg@bottlepost.me>
2026-05-14 20:39:51 +0200

    Typo fix in 20260401 update rated tables.sql

--- MiniMediaSonicServer.Api/DbScripts/20260401 update rated tables.sql
+++ MiniMediaSonicServer.Api/DbScripts/20260401 update rated tables.sql
@@ -1,3 +1,3 @@
 alter table sonicserver_album_rated add column if not exists UnStarredAt timestamp;
 alter table sonicserver_artist_rated add column if not exists UnStarredAt timestamp;
-alter table sonicserver_track_rated add columnif not exists  UnStarredAt timestamp;
+alter table sonicserver_track_rated add column if not exists  UnStarredAt timestamp;
