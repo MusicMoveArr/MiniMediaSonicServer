7979285fc0363047fc4d17b5932e68b9ebc25cb7
MusicMoveArr <jys78dfg@bottlepost.me>
2026-05-14 20:48:18 +0200

    Commit 200 is a typo fix...

--- MiniMediaSonicServer.Api/DbScripts/20260426 improved precomputed genres.sql
+++ MiniMediaSonicServer.Api/DbScripts/20260426 improved precomputed genres.sql
@@ -1,6 +1,6 @@
 ALTER TABLE metadata if exists drop COLUMN computed_genre;
 
-ALTER TABLE metadata if not exists ADD COLUMN if not exists computed_genre text GENERATED ALWAYS AS (
+ALTER TABLE metadata ADD COLUMN if not exists computed_genre text GENERATED ALWAYS AS (
     regexp_replace(tag_alljsontags->>'Genre', '[,/&|]', ';', 'g')
 ) STORED;
 
